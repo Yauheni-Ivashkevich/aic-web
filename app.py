993814b4b7a046c4bf390a9703aafbee80e21d4f
Yauheni Ivashkevich <zheklex@ya.ru>
2021-10-01 18:42:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ from resources.routes import initialize_routes
 
 app = Flask(__name__)
 api = Api(app)
-bcrypt = Bcrypt(app) 
+bcrypt = Bcrypt(app)
 
 app.config['MONGODB_SETTINGS'] = {'host': 'mongodb://localhost/aic-web'}
+# app.config['MONGODB_HOST'] = "mongodb+srv://eugene_ivashkevich:wpLV8ZJcC1spQoc6@aic-web.u94a7.mongodb.net/aic-web?retryWrites=true&w=majority" 
 
 initialize_db(app)
 initialize_routes(api)
